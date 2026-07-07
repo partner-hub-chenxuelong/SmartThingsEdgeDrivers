ea972811adaa5a0491b60008354de3454903ceca
xulong.chen <xuelong.chen@partner.samsung.com>
2026-07-07 20:15:26 +0800

    update_roombeautiful

--- drivers/SmartThings/zigbee-window-treatment/src/rooms-beautiful/init.lua
+++ drivers/SmartThings/zigbee-window-treatment/src/rooms-beautiful/init.lua
@@ -11,11 +11,15 @@ local PowerConfiguration = zcl_clusters.PowerConfiguration
 local OnOff = zcl_clusters.OnOff
 local WindowCovering = zcl_clusters.WindowCovering
 
-
+local utils = require "st.utils"
 local INVERT_CLUSTER = 0xFC00
 local INVERT_CLUSTER_ATTRIBUTE = 0x0000
 local PREV_TIME = "shadeLevelCmdTime"
 
+
+local LATEST_TARGET_LEVEL = "latest_target_level"
+local TARGET_LEVEL_TIME_OUT = "_target_level_timeout"
+local TARGET_LEVEL_TIME_OUT_SECONDS = 30 
 
 local function invert_preference_handler(device)
   local window_level = device:get_latest_state("main", capabilities.windowShadeLevel.ID, capabilities.windowShadeLevel.shadeLevel.NAME) or 0
@@ -40,6 +44,17 @@ end
 
 local function current_position_attr_handler(driver, device, value, zb_rx)
   local level = value.value
+  local latest_target_level = device:get_field(LATEST_TARGET_LEVEL)
+  if  latest_target_level ~= nil then
+    if utils.round(level) == utils.round(latest_target_level) then
+      device:set_field(LATEST_TARGET_LEVEL, nil)
+      local timer = device:get_field(TARGET_LEVEL_TIME_OUT)
+      if timer ~= nil then
+        device.thread:cancel_timer(timer)
+        device:set_field(TARGET_LEVEL_TIME_OUT, nil)
+      end
+    end
+  end
   local window_shade = capabilities.windowShade.windowShade
   device:emit_event(capabilities.windowShadeLevel.shadeLevel(level))
   if level == 0 or level == 100 then
@@ -80,6 +95,39 @@ local do_refresh = function(self, device)
   device:send(invert_cluster_read)
 end
 
+local function window_shade_step_level_cmd(driver, device, command)
+  local step = command.args.stepSize or command.args[1]
+
+  local latest_target_level = device:get_field(LATEST_TARGET_LEVEL)
+  local current_level = latest_target_level or
+    device:get_latest_state("main", capabilities.windowShadeLevel.ID,
+      capabilities.windowShadeLevel.shadeLevel.NAME) or 0
+
+  local target_level = current_level + step
+  if target_level > 100 then
+    target_level = 100
+  elseif target_level < 0 then
+    target_level = 0
+  end
+  target_level = utils.round(target_level)
+
+  device:set_field(LATEST_TARGET_LEVEL, target_level)
+
+  local old_timer = device:get_field(TARGET_LEVEL_TIME_OUT)
+  if old_timer ~= nil then
+    device.thread:cancel_timer(old_timer)
+  end
+
+  local timer = device.thread:call_with_delay(TARGET_LEVEL_TIME_OUT_SECONDS, function(d)
+    device:set_field(LATEST_TARGET_LEVEL, nil)
+    device:set_field(TARGET_LEVEL_TIME_OUT, nil)
+  end)
+  device:set_field(TARGET_LEVEL_TIME_OUT, timer)
+
+  local new_command = { args = { shadeLevel = target_level }, component = command.component }
+  window_shade_level_cmd(driver, device, new_command)
+end
+
 local rooms_beautiful_handler = {
   NAME = "Rooms Beautiful Device Handler",
   capability_handlers = {
@@ -89,6 +137,9 @@ local rooms_beautiful_handler = {
     [capabilities.windowShade.ID] = {
       [capabilities.windowShade.commands.open.NAME] = build_window_shade_cmd(OnOff.server.commands.On),
       [capabilities.windowShade.commands.close.NAME] = build_window_shade_cmd(OnOff.server.commands.Off),
+    },
+    [capabilities.statelessWindowShadeLevelStep.ID] = {
+      [capabilities.statelessWindowShadeLevelStep.commands.stepShadeLevel.NAME] = window_shade_step_level_cmd
     },
     [capabilities.refresh.ID] = {
       [capabilities.refresh.commands.refresh.NAME] = do_refresh,
