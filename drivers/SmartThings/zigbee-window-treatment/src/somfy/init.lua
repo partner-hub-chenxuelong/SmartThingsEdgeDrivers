d5c2c9e5e7a6b9ee4ca0515744d0e2b14bcb4cbc
xulong.chen <xuelong.chen@partner.samsung.com>
2026-07-07 18:54:13 +0800

    update_somfy

--- drivers/SmartThings/zigbee-window-treatment/src/somfy/init.lua
+++ drivers/SmartThings/zigbee-window-treatment/src/somfy/init.lua
@@ -14,6 +14,9 @@ local GLYDEA_MOVE_THRESHOLD = 3
 local MOVE_LESS_THAN_THRESHOLD = "_sameLevelEvent"
 local FINAL_STATE_POLL_TIMER = "_finalStatePollTimer"
 
+local LATEST_TARGET_LEVEL = "latest_target_level"
+local TARGET_LEVEL_TIME_OUT = "_target_level_timeout"
+local TARGET_LEVEL_TIME_OUT_SECONDS = 30 
 
 local function overwrite_existing_timer_if_needed(device, new_timer)
   local old_timer = device:get_field(FINAL_STATE_POLL_TIMER)
@@ -26,6 +29,17 @@ end
 local function current_position_attr_handler(driver, device, value, zb_rx)
   -- Somfy Device report as invert value
   local level = 100 - value.value
+  local latest_target_level = device:get_field(LATEST_TARGET_LEVEL)
+  if  latest_target_level ~= nil then
+    if utils.round(level) == utils.round(latest_target_level) then
+      device:set_field(LATEST_TARGET_LEVEL, nil)
+      local timer = device:get_field(TARGET_LEVEL_TIME_OUT)
+      if timer ~= nil then
+        device.thread:cancel_timer(timer)
+        device:set_field(TARGET_LEVEL_TIME_OUT, nil)
+      end
+    end
+  end
   local current_level = device:get_latest_state(device:get_component_id_for_endpoint(zb_rx.address_header.src_endpoint.value),
     capabilities.windowShadeLevel.ID, capabilities.windowShadeLevel.shadeLevel.NAME)
   local windowShade = capabilities.windowShade.windowShade
@@ -90,12 +104,47 @@ local function window_shade_preset_cmd(driver, device, command)
   command.args.shadeLevel = level
   window_shade_level_cmd(driver, device, command)
 end
+local function window_shade_step_level_cmd(driver, device, command)
+  local step = command.args.stepSize or command.args[1]
+
+  local latest_target_level = device:get_field(LATEST_TARGET_LEVEL)
+  local current_level = latest_target_level or
+    device:get_latest_state("main", capabilities.windowShadeLevel.ID,
+      capabilities.windowShadeLevel.shadeLevel.NAME) or 0
+
+  local target_level = current_level + step
+  if target_level > 100 then
+    target_level = 100
+  elseif target_level < 0 then
+    target_level = 0
+  end
+  target_level = utils.round(target_level)
+
+  device:set_field(LATEST_TARGET_LEVEL, target_level)
+
+  local old_timer = device:get_field(TARGET_LEVEL_TIME_OUT)
+  if old_timer ~= nil then
+    device.thread:cancel_timer(old_timer)
+  end
+
+  local timer = device.thread:call_with_delay(TARGET_LEVEL_TIME_OUT_SECONDS, function(d)
+    device:set_field(LATEST_TARGET_LEVEL, nil)
+    device:set_field(TARGET_LEVEL_TIME_OUT, nil)
+  end)
+  device:set_field(TARGET_LEVEL_TIME_OUT, timer)
+
+  local new_command = { args = { shadeLevel = target_level }, component = command.component }
+  window_shade_level_cmd(driver, device, new_command)
+end
 
 local somfy_handler = {
   NAME = "SOMFY Device Handler",
   capability_handlers = {
     [capabilities.windowShadeLevel.ID] = {
       [capabilities.windowShadeLevel.commands.setShadeLevel.NAME] = window_shade_level_cmd
+    },
+    [capabilities.statelessWindowShadeLevelStep.ID] = {
+      [capabilities.statelessWindowShadeLevelStep.commands.stepShadeLevel.NAME] = window_shade_step_level_cmd
     },
     [capabilities.windowShadePreset.ID] = {
       [capabilities.windowShadePreset.commands.presetPosition.NAME] = window_shade_preset_cmd
